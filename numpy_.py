61e401e0ac1e29d0c1c2462f70612a806a6f48b8
Cqk <cqk0100@gmail.com>
2017-07-02 23:22:56 +0800

    add numpy simple practices. not finish...

---
+++ numpy_.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import numpy as np
+
+
+# fibonacci function of python version
+def fibonacci(n):
+	fib = np.mat([[1, 1], [1, 0]])
+	ans = (fib ** n)[0, 0]
+	return ans
+
+
+def quick_pow(a: int, n: int) -> int:
+	ans = 1
+	while n:
+		if n & 1:  # n%2
+			ans *= a
+		n = int(n >> 1)
+		a *= a
+	return ans
+
+
+# quick powder of matrix
+def quick_pow_mat(mat, n):
+	'''
+	:param mat: the input matrix
+	:param n: the exponent
+	:return:the calculated matrix
+	'''
+	assert (mat.shape)[0] == (mat.shape)[1] and n is not 0, "Invalid input data ."
+	ans = np.eye((mat.shape)[0])
+	while n:
+		if n & 1:
+			ans = np.multiply(ans, mat)
+		n = int(n >> 1)
+		mat = np.multiply(mat, mat)
+	return ans
