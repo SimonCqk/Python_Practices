d6919512239889490c781cdea8d2dcec23bab006
Cqk <cqk0100@gmail.com>
2017-12-01 19:51:39 +0800

    re-correct TCP socket example.

--- socket/simpleTCPclient.py
+++ socket/simpleTCPclient.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import socket
 import time
-from socket import *
 
-now = lambda: time.time()
+
+def now(): return time.time()
+
 
 HOST = '127.0.0.1'  # or 'localhost'
 PORT = 21567  # same as server port.
 BUFSIZE = 10240
 ADDR = (HOST, PORT)
 
-tcp_clt_sock = socket(AF_INET, SOCK_STREAM)
+tcp_clt_sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 tcp_clt_sock.connect(ADDR)
 
 while True:
-	data = input('data >:')
-	if not data:
-		break
-	tcp_clt_sock.send(data)
-	data = tcp_clt_sock.recv(BUFSIZE)
-	if not data:
-		break
-	print(data.decode('utf-8'))
+    data = input( 'data >:' )
+    if not data:
+        break
+    tcp_clt_sock.send( data.encode( 'utf-8' ) )
+    data = tcp_clt_sock.recv( BUFSIZE )
+    if not data:
+        break
+    print( data.decode( 'utf-8' ) )
 
 tcp_clt_sock.close()
--- socket/simpleTCPserver.py
+++ socket/simpleTCPserver.py
@@ -3,29 +3,38 @@
 '''
  run before client.
 '''
+import socket
+import threading
 import time
-from socket import *
 
-now = lambda: time.time()
+
+def now(): return time.time()
+
+
+def tcp_link_worker(sock, addr):
+    print( 'connected from ...', addr )
+    while True:
+        data = sock.recv( BUFSIZE )
+        print( 'received: ', data.decode( 'utf-8' ) )
+        if not data:
+            break
+        sock.send( '[{0}] {1}'.format( now(), data ).encode( 'utf-8' ) )
+    sock.close()
+
 
 HOST = ''
 PORT = 21567
 BUFSIZE = 10240
 ADDR = (HOST, PORT)
 
-tcp_ser_sock = socket(AF_INET, SOCK_STREAM)
+tcp_ser_sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 tcp_ser_sock.bind(ADDR)
 tcp_ser_sock.listen(5)  # max income connection is 5.
 
-while True:
-	print('waiting for connection ...')
-	tcp_cli_sock, addr = tcp_ser_sock.accept()
-	print('connected from ...', addr)
-	while True:
-		data = tcp_cli_sock.recv(BUFSIZE)
-		if not data:
-			break
-		tcp_cli_sock.send('[{0}] {1}'.format(bytes(now(), 'utf-8'), data))
-	tcp_cli_sock.close()
 
+while True:
+    print( 'waiting for connection ...' )
+    tcp_cli_sock, addr = tcp_ser_sock.accept()
+    t = threading.Thread( target=tcp_link_worker, args=(tcp_cli_sock, addr) )
+    t.start()
 tcp_ser_sock.close()  # never execute.
