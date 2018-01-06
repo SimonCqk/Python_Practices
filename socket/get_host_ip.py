eac8a0e5ab5356e1bb0656e83dd79a410f688518
Cqk <cqk0100@gmail.com>
2018-01-06 16:55:39 +0800

    add 2 implementation of visitor pattern.[recursive & non-recursive]

--- socket/get_host_ip.py
+++ socket/get_host_ip.py
@@ -3,6 +3,6 @@ import socket
 
 def get_host_ip():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(('8.8.8.8', 80))
+        s.connect(('8.8.8.8', 80))  # default port of HTTP.
         ip = s.getsockname()[0]
     return ip
